f8d76de308e13e8e5df05775b3282e684ef64d89
FeiZhan <dog217@126.com>
2013-08-17 17:09:18 -0700

    leetcode

--- leetcode/Spiral Matrix.cpp
+++ leetcode/Spiral Matrix.cpp
@@ -1,3 +1,7 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int> > &matrix) {
@@ -20,17 +24,63 @@ public:
 		// right
 		for (int i = level + 1; i < matrix.size() - level; ++ i)
 		{
-			ans.push_back(matrix[i][matrix[0].size() - level]);
+			ans.push_back(matrix[i][matrix[0].size() - level - 1]);
 		}
-		// down
-		for (int i = level + 1; i < matrix[0].size() - level; ++ i)
+		if (matrix.size() > level * 2 + 1)
 		{
-			ans.push_back(matrix[matrix.size() - level][matrix[0].size() - i]);
+		cout << "levelc " << level << endl;
+			// down
+			for (int i = level + 1; i < matrix[0].size() - level; ++ i)
+			{
+		cout << " " << matrix[matrix.size() - level - 1][matrix[0].size() - i - 1] << endl;
+				ans.push_back(matrix[matrix.size() - level - 1][matrix[0].size() - i - 1]);
+			}
 		}
-		// left
-		for (int i = level + 1; i < matrix.size() - level; ++ i)
+		if (matrix[0].size() > level * 2 + 1)
 		{
-			ans.push_back(matrix[i][matrix[0].size() - level]);
+			// left
+			for (int i = level + 1; i < matrix.size() - level - 1; ++ i)
+			{
+				ans.push_back(matrix[matrix.size() - i - 1][level]);
+			}
 		}
+		vector<int> child = spiral(matrix, level + 1);
+		ans.reserve( ans.size() + child.size() );
+		ans.insert( ans.end(), child.begin(), child.end() );
+		return ans;
 	}
 };
+int main()
+{
+	Solution s;
+	vector<vector<int> > matrix;
+	vector<int> tmp;
+	//[[2,3]]
+	//[[1,2],[3,4]]
+	//[[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+	tmp.clear();
+	tmp.push_back(1);
+	tmp.push_back(2);
+	tmp.push_back(3);
+	tmp.push_back(4);
+	matrix.push_back(tmp);
+	tmp.clear();
+	tmp.push_back(5);
+	tmp.push_back(6);
+	tmp.push_back(7);
+	tmp.push_back(8);
+	matrix.push_back(tmp);
+	tmp.clear();
+	tmp.push_back(9);
+	tmp.push_back(10);
+	tmp.push_back(11);
+	tmp.push_back(12);
+	matrix.push_back(tmp);
+	vector<int> ans = s.spiralOrder(matrix);
+	for (int i = 0; i < ans.size(); ++ i)
+	{
+		cout << ans[i] << " ";
+	}
+	cout << endl;
+	return 0;
+}
