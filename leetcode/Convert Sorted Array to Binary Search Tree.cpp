dbfa8c34c736e3db2aef9dcd425ef9c2dabbfead
unknown <t-fezhan@T-FEZHAN-PC2.northamerica.corp.microsoft.com>
2014-04-17 16:26:10 -0700

    leetcode

--- leetcode/Convert Sorted Array to Binary Search Tree.cpp
+++ leetcode/Convert Sorted Array to Binary Search Tree.cpp
@@ -30,3 +30,22 @@ public:
 		return root;
     }
 };
+// 2014-04-17
+class Solution {
+public:
+	TreeNode *sortedArrayToBST(vector<int> &num) {
+		return sortedArrayToBST(num, 0, num.size());
+	}
+	TreeNode *sortedArrayToBST(const vector<int> &num, int left, int right)
+	{
+		if (left >= right)
+		{
+			return NULL;
+		}
+		int mid = (left + right) / 2;
+		TreeNode *root(new TreeNode(num[mid]));
+		root->left = sortedArrayToBST(num, left, mid);
+		root->right = sortedArrayToBST(num, mid + 1, right);
+		return root;
+	}
+};
